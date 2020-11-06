e1811636382b19e173fca5b59c8f875040b5c3e2
haro99 <45450954+haro99@users.noreply.github.com>
2020-11-06 20:27:47 +0900

    Add files via upload
    Base code

---
+++ Title.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+
+    // Update is called once per frame
+    void Update () {
+
+        //スペースキーを押したかまたはスマホ画面でタッチされたらゲームシーンへ行く
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space!");
+            SceneManager.LoadScene("SampleScene");
+        }
+        else if (Input.touchCount > 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+	}
+}
---
+++ Titleback.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titleback : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("titlescene");
+    }
+}
